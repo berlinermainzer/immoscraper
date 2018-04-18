997b1ad9df83f5b40c04ea5ea765706ee61570c1
Johannes Felten <johannesfelten@gmail.com>
2018-04-18 22:22:24 +0200

    enhance beyer spider

--- spiders/beyer_spider.py
+++ spiders/beyer_spider.py
@@ -9,8 +9,13 @@ class BeyerSpider(scrapy.Spider):
 
     def parse(self, response):
         for offer in response.xpath("//div[@class='col-sm-6 col-md-4 immothumbs']"):
-            print(offer)
+            #print(offer)
             yield {
-                'title': offer.xpath("h3/text()").extract_first().strip(),
-                'url': offer.xpath('a/@href').extract_first()
+                'title': offer.xpath('h3/text()').extract_first().strip(),
+                'url': offer.xpath('a/@href').extract_first(),
+                'price': offer.xpath("div[1]/div[1]/div[2]/text()").extract_first().replace("€ ", "").replace(".", ""),
+                'type': offer.xpath("div[1]/div[2]/div[2]/text()").extract_first(),
+                'size_house':  offer.xpath("div[1]/div[3]/div[2]/text()").extract_first().replace(" m²", ""),
+                'size_ground':  offer.xpath("div[1]/div[4]/div[2]/text()").extract_first().replace(" m²", ""),
+                'picture_url': offer.xpath('a/img/@src').extract_first(),
             } 
