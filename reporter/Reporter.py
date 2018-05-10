0e3926a964d3a96130c661433b8bcad0f765f8d7
Johannes Felten <johannesfelten@gmail.com>
2018-05-10 12:10:07 +0200

    Pull data from mongodb, revamp data format

--- reporter/Reporter.py
+++ reporter/Reporter.py
@@ -3,6 +3,9 @@ from jinja2 import Environment, FileSystemLoader
 import os
 import sys
 import logging
+import pymongo
+import pprint
+
 
 logging.basicConfig()
 logger = logging.getLogger('Reporter')
@@ -10,16 +13,22 @@ logger.setLevel(10)
 
 class Reporter(object):
     
-    def __init__(self, templatedir='templates'):
 
+    def __init__(self, templatedir='templates', mongo_uri='mongodb://localhost', mongo_db = 'immodb', mongo_collection_name = 'items'):
         if os.path.isdir(templatedir):
             self.templatedir = templatedir
         else:
             self.templatedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), templatedir)
         self.env = Environment(loader=FileSystemLoader(self.templatedir))
 
+        self.mongo_client = pymongo.MongoClient(mongo_uri)
+        self.mongo_db = self.mongo_client[mongo_db]
+        self.mongo_collection = self.mongo_db[mongo_collection_name]
 
-    def _mailrender(self, data, template):
+    def close(self):
+        self.mongo_client.close()
+
+    def mailrender(self, data, template):
         template = template + ".tmpl"
         logger.info("Rendering template '%s'" % (template))
         text = self.env.get_template(template)
@@ -27,32 +36,30 @@ class Reporter(object):
 
         logger.info(msg)
 
+    def getData(self):
 
+        data = [
+            { "subject" : "ImmoScraper Report" },
+            { "email_receivers" : ["xyz@gmail.com", "abc@gmail.com"] }
+        ]
+
+        for el in self.mongo_collection.find({'url_hash': 'ffd8505ea2340807fbb0e49ff1a5d23d'}):
+            data.append({"entries" : el})
+
+        return data
 
 def main(argv):
     logger.info(argv)
-    
-    elist = [{
-        "email": "xyz@gmail.com",
-        "subject": "ImmoScraper Report",
-        "data" : { 
-            "dear": "Johannes",
-            "msg": "Hola mundo"
-        }
-    },
-        {
-        "email": "abc@gmail.com",
-        "subject": "ImmoScraper Report",
-        "data" : { 
-            "dear": "Kerstin",
-            "msg": "Na, du Süße?"
-        }
-    }]
-
+        
     reporter = Reporter()
 
+    elist = reporter.getData()
+
     for el in elist:
-        reporter._mailrender(el['data'], 'report')
+        pprint.pprint(el)
+        #reporter.mailrender(el['data'], 'report')
+
+    reporter.close()
 
 if __name__ == "__main__":
     main(sys.argv)
