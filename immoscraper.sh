6c7a8c75b99f552a67bd31206d1c7222c0de3fcc
Johannes Felten <johannesfelten@gmail.com>
2019-01-05 20:41:56 +0100

    Add sending of mails

--- immoscraper.sh
+++ immoscraper.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-echo ImmoScraper starting.
+echo ####### ImmoScraper starting.
 
 
-echo Perform scraping.
+echo ####### Perform scraping.
 for spider in `find spiders -type f -name "*Spider.py" -exec basename {} .py ';';`; do
     echo Running $spider
     scrapy crawl $spider
 done
-echo Scraping done
+echo ####### Scraping done
 
+echo ####### Sending mails
+python reporter/Reporter.py $1 $2
+echo ####### Sending mails done
 
-
-
-
-echo ImmoScraper done.
+echo ####### ImmoScraper done.
